fb59cfa6f5221f2b17cea2e453168de9f925eba0
Frank Lemanschik <frank@github.dspeed.eu>
2017-11-06 05:08:10 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+  Helps to Stealify Things :)
+
+
+*/
+
+module.exports = function stealify(strOobj,match,replacment) {
+  //TODO: if Obj walk keys no functions 
+  //TODO: later wrapp functions identify promises, function replace return values
+  //TODO: find files with pattern
+  //replace pattern in files
+  //uvm
+  
+}
